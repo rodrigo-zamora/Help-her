fb844c58c4e9cbcd486ca98463b51a7cd5c36987
Rodrigo Zamora <rodrigo_zamora_dav@hotmail.com>
2021-03-23 01:51:10 -0600

    Added debug messages

--- src/main/KeyboardInput.java
+++ src/main/KeyboardInput.java
@@ -13,16 +13,17 @@ public class KeyboardInput extends KeyAdapter {
 
     public void keyPressed(KeyEvent event) {
         int key = event.getKeyCode();
+        System.out.println("keyPressed: " + key);
         for(int i = 0; i < handler.object.size(); i++){
             GameObject tempObject = handler.object.get(i);
             if(tempObject.getId() == ID.Player){
                 if(key == 38){ // 38 = flecha arriba
                     tempObject.setSpeedY(-tempObject.SPEED);
-                } else if(key == 40){ // 40 = flecha abajo
+                } if(key == 40){ // 40 = flecha abajo
                     tempObject.setSpeedY(tempObject.SPEED);
-                } else if(key == 39){ // 39 = flecha derecha
+                } if(key == 39){ // 39 = flecha derecha
                     tempObject.setSpeedX(tempObject.SPEED);
-                } else if(key == 37) { // 37 = flecha izquierda
+                } if(key == 37) { // 37 = flecha izquierda
                     tempObject.setSpeedX(-tempObject.SPEED);
                 }
             }
@@ -31,16 +32,17 @@ public class KeyboardInput extends KeyAdapter {
 
     public void keyReleased(KeyEvent event) {
         int key = event.getKeyCode();
+        System.out.println("keyReleased: " + key);
         for(int i = 0; i < handler.object.size(); i++){
             GameObject tempObject = handler.object.get(i);
             if(tempObject.getId() == ID.Player){
                 if(key == 38){ // 38 = flecha arriba
                     tempObject.setSpeedY(0);
-                } else if(key == 40){ // 40 = flecha abajo
+                } if(key == 40){ // 40 = flecha abajo
                     tempObject.setSpeedY(0);
-                } else if(key == 39){ // 39 = flecha derecha
+                } if(key == 39){ // 39 = flecha derecha
                     tempObject.setSpeedX(0);
-                } else if(key == 37) { // 37 = flecha izquierda
+                } if(key == 37) { // 37 = flecha izquierda
                     tempObject.setSpeedX(0);
                 }
             }
