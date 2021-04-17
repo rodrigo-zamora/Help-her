017896d9beeb4f23d339b869dc6e5c9b57b39cb0
Jose Jaime Gtz <jose_jaime@live.com.mx>
2021-04-17 03:55:20 -0500

    Credit & Start Buttons

--- src/com/game/juanito/screen/screens/MainMenu.java
+++ src/com/game/juanito/screen/screens/MainMenu.java
@@ -7,13 +7,16 @@ import java.net.URL;
 
 public class MainMenu {
 
+    public static final int WIDTH = 1080;
+    public static final int HEIGHT = 640;
+
     private static final Toolkit toolkit = Toolkit.getDefaultToolkit();
     private static final URL background = ClassLoader.getSystemResource("Menu/IntroLarge.gif");
     private static final Image backgroundImage = toolkit.getImage(background);
-    private static final URL start = ClassLoader.getSystemResource("Menu/Empezar.png");
-    private static final Image startImage = toolkit.getImage(start);
-    private static final URL credits = ClassLoader.getSystemResource("Menu/Creditos.png");
-    private static final Image creditsImage = toolkit.getImage(credits);
+    private static final URL startButton = ClassLoader.getSystemResource("Menu/StartButtonPixel4.png");
+    private static final Image startImage = toolkit.getImage(startButton);
+    private static final URL creditsButton = ClassLoader.getSystemResource("Menu/CreditsButtonPixel4.png");
+    private static final Image creditsImage = toolkit.getImage(creditsButton);
 
     public static void render(Graphics graphics) {
 
@@ -26,15 +29,15 @@ public class MainMenu {
 
         graphics.drawImage(
                 startImage,
-                100,
-                200,
+                (WIDTH/4) - (startImage.getWidth(null)/2),
+                (HEIGHT/2) - (startImage.getHeight(null)/2),
                 null
         );
 
         graphics.drawImage(
                 creditsImage,
-                100,
-                300,
+                (WIDTH/2) + (WIDTH/4) - (creditsImage.getWidth(null)/2),
+                (HEIGHT/2) - (creditsImage.getHeight(null)/2),
                 null
         );
 
