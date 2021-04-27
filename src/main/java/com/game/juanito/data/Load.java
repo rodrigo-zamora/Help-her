0751f519257bd205d2741fb137c8bc71cd12f4be
Rodrigo Zamora <rodrigo_zamora_dav@hotmail.com>
2021-04-27 01:08:31 -0500

    Added json dependency to maven

---
+++ src/main/java/com/game/juanito/data/Load.java
@@ -0,0 +1,4 @@
+package com.game.juanito.data;
+
+public class Load {
+}
---
+++ src/main/java/com/game/juanito/data/Save.java
@@ -0,0 +1,10 @@
+package com.game.juanito.data;
+
+import org.json.JSONObject;
+
+public class Save {
+
+    public static void saveGame() {
+        JSONObject jsonObject = new JSONObject();
+    }
+}
