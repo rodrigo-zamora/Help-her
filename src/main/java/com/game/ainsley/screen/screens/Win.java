db83c82a1fe55543e9f54955b431191455b66ed4
Jose Jaime Gtz <jose_jaime@live.com.mx>
2021-05-10 04:00:57 -0500

    WIN screen

--- src/main/java/com/game/ainsley/screen/screens/Win.java
+++ src/main/java/com/game/ainsley/screen/screens/Win.java
@@ -1,18 +1,27 @@
 package com.game.ainsley.screen.screens;
 
+import com.game.ainsley.main.Game;
 import lib.ainsley.FileManager;
 
 import java.awt.*;
 
 public class Win {
 
-    private static final Image backgroundImage = FileManager.loadImage("screens/winScreen.gif");
+    private static final Image backgroundImage = FileManager.loadImage("screens/audioScreen.png");
+    private static final Image menuImage = FileManager.loadImage("buttons/menuButtonPixel4.png");
 
     public static void render(Graphics graphics) {
         graphics.drawImage(
                 backgroundImage,
                 0,
                 0,
+                null
+        );
+
+        graphics.drawImage(
+                menuImage,
+                (Game.WIDTH / 2) - (menuImage.getWidth(null) / 2),
+                (Game.HEIGHT / 5) * 3,
                 null
         );
     }
